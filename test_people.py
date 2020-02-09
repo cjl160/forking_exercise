0ea3d8bd3aff05a26152abfbc2282ec2884602ca
Oliver Smart <oliver.s.smart@gmail.com>
2020-02-09 16:36:22 +0000

    Pytest for people (not yet written) fails TDD
    see issue #1
    
    $ pytest
    ============================= test session starts =============================
    platform darwin -- Python 3.7.1, pytest-4.0.2, py-1.7.0, pluggy-0.8.0
    rootdir: /Users/osmart/Documents/ARU/forking_exercise, inifile:
    plugins: remotedata-0.3.1, openfiles-0.3.1, doctestplus-0.2.0, arraydiff-0.3
    collected 0 items / 1 errors
    
    =================================== ERRORS ====================================
    _______________________ ERROR collecting test_people.py _______________________
    ImportError while importing test module '/Users/osmart/Documents/ARU/forking_exercise/test_people.py'.
    Hint: make sure your test modules/packages have valid Python names.
    Traceback:
    test_people.py:2: in <module>
        from people import setup_people
    E   ModuleNotFoundError: No module named 'people'
    !!!!!!!!!!!!!!!!!!! Interrupted: 1 errors during collection !!!!!!!!!!!!!!!!!!!
    =========================== 1 error in 0.11 seconds ===========================

---
+++ test_people.py
@@ -0,0 +1,14 @@
+""" pytest tests for people dictionary"""
+from people import setup_people
+
+
+def test_oliver():
+    oliver = setup_people()['Oliver_Smart']
+    assert oliver.first_name == 'Oliver'
+    assert oliver.age == 21
+    assert oliver.hobbies == []
+
+
+def test_all_keys_match_names():
+    for key, person in setup_people().items():
+        assert key == f'{person.first_name}_{person.surname}'
