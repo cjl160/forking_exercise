fa8e1171d50f15599dfd3180f5a03d1e50cfa73e
Oliver Smart <oliver.s.smart@gmail.com>
2020-02-09 17:28:16 +0000

    people module with data for oliver
    for Github issue #1
    passes pytest locally so should sort #2

---
+++ people.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""people dictionary to showoff namedtuple"""
+from collections import namedtuple
+PERSON = namedtuple('person', 'first_name surname age pet hobbies')
+
+
+def setup_people():
+    """returns a dictionary of people with
+       key firstname_surname: value: namedtuple PERSON
+       that has firstname, surname, age, pet, hobbies items
+    """
+    people = {}
+    _add_oliver(people)
+    return people
+
+
+def _add_oliver(people):
+    """ adds oliver """
+    new_p = PERSON(first_name='Oliver', surname='Smart',
+                   age=21, pet='Rat', hobbies=[])
+    key = new_p.first_name + '_' + new_p.surname
+    people[key] = new_p
+
+def main():
+    """ main function run as script """
+    people = setup_people()
+    print(people)
+
+
+if __name__ == '__main__':
+    main()
