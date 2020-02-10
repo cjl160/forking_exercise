e3fc5ca8a37fd92d622984fe159ab6798617949c
jakeHooleyARU <jake.hooley@student.anglia.ac.uk>
2020-02-10 09:36:12 +0000

    add test for squanchy, fails TDD #7

--- test_people.py
+++ test_people.py
@@ -9,6 +9,14 @@ def test_oliver():
     assert oliver.hobbies == []
 
 
+def test_squanchy():
+    squanchy = setup_people()['squanchy_rick and morty']
+    assert oliver.first_name == 'squanchy'
+    assert oliver.age == 21
+    assert oliver.hobbies == ['squanching']
+
+
+
 def test_all_keys_match_names():
     for key, person in setup_people().items():
         assert key == f'{person.first_name}_{person.surname}'
