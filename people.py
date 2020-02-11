893bd58c081744c70761ca743ef5c83ec60d2801
sffrn <saffron_writer@outlook.com>
2020-02-11 13:46:00 +0000

    Added Eeyore to people.py following ARU-Bioinf-ISE#13

--- people.py
+++ people.py
@@ -11,7 +11,7 @@ def setup_people():
     """
     people = {}
     _add_oliver(people)
-    # TODO add another person - Eeyore
+    _add_eeyore(people)
     return people
 
 
@@ -19,6 +19,13 @@ def _add_oliver(people):
     """ adds oliver """
     new_p = PERSON(first_name='Oliver', surname='Smart',
                    age=21, pet='Rat', hobbies=[])
+    key = new_p.first_name + '_' + new_p.surname
+    people[key] = new_p
+
+def _add_eeyore(people):
+    """Adds eeyore"""
+    new_p = PERSON(first_name='Eeyore', surname='Milne',
+                    age=98, pet='', hobbies=['Being gloomy, eating thistles and sugar cubes, playing Poohsticks, going to birthday parties, flowers, stars, sad stories and poems, looking over his hill'])
     key = new_p.first_name + '_' + new_p.surname
     people[key] = new_p
 
