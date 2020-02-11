6e656944a8908e3b195d453834a9b927fded2e40
sffrn <saffron_writer@outlook.com>
2020-02-11 12:09:24 +0000

    Added Eeyore to test_people and ran pytest to ensure it fails.

--- test_people.py
+++ test_people.py
@@ -8,6 +8,11 @@ def test_oliver():
     assert oliver.age == 21
     assert oliver.hobbies == []
 
+def test_eeyore():
+	eeyore = setup_people()['Eeyore_Milne']
+	assert eeyore.first_name == 'Eeyore'
+	assert eeyore.age == 98
+	assert eeyore.hobbies == ['Being gloomy, eating thistles and sugar cubes, Poohsticks, birthday parties, flowers, stars, sad stories and poems, looking over his hill']
 
 def test_all_keys_match_names():
     for key, person in setup_people().items():
