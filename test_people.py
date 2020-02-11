9875322cbea771f923a716ac0f103b7d4bf14335
sffrn <saffron_writer@outlook.com>
2020-02-11 13:48:52 +0000

    Made some minor changes to test_people.py so that is matches the information in people.py ARU-Bioinf-ISE#13

--- test_people.py
+++ test_people.py
@@ -12,7 +12,7 @@ def test_eeyore():
 	eeyore = setup_people()['Eeyore_Milne']
 	assert eeyore.first_name == 'Eeyore'
 	assert eeyore.age == 98
-	assert eeyore.hobbies == ['Being gloomy, eating thistles and sugar cubes, Poohsticks, birthday parties, flowers, stars, sad stories and poems, looking over his hill']
+	assert eeyore.hobbies == ['Being gloomy, eating thistles and sugar cubes, playing Poohsticks, going to birthday parties, flowers, stars, sad stories and poems, looking over his hill']
 
 def test_all_keys_match_names():
     for key, person in setup_people().items():
