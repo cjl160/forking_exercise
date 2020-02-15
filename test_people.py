2ff55c893ecc462e1cbeb67e31e553c356ae812e
Chris <cc1325@student.anglia.ac.uk>
2020-02-15 19:00:05 +0000

    added test which fails #19

--- test_people.py
+++ test_people.py
@@ -8,6 +8,12 @@ def test_oliver():
     assert oliver.age == 21
     assert oliver.hobbies == []
 
+def test_cartman():
+    cartman = setup_people()['Eric Cartman']
+    assert cartman.first_name == 'Eric'
+    assert cartman.age == 10
+    assert cartman.hobbies == []
+
 
 def test_all_keys_match_names():
     for key, person in setup_people().items():
