751fc9697e9de575581ff3f257acf89c9e97b64b
jakeHooleyARU <57401627+jakeHooleyARU@users.noreply.github.com>
2020-02-25 11:53:17 +0000

    updated test_people.py to fix pep8

--- test_people.py
+++ test_people.py
@@ -13,10 +13,9 @@ def test_squanchy():
     squanchy = setup_people()['squanchy_smith']
     assert squanchy.first_name == 'squanchy'
     assert squanchy.age == 21
-    assert squanchy.hobbies == ['squanching','running']
-
-
-
+    assert squanchy.hobbies == ['squanching', 'running']
+    
+    
 def test_all_keys_match_names():
     for key, person in setup_people().items():
         assert key == f'{person.first_name}_{person.surname}'
