2543b1d4d3e03a3f8109c4e979d3007d4931b30f
Oliver Smart <oliver.s.smart@gmail.com>
2020-03-08 18:02:21 +0000

    Make script output pretty to show the apprentices
    (ARU) $ python people.py
    Oliver          Smart           age:21     pet:Rat             hobbies: []
    Road            Runner          age:71     pet:(none)          hobbies: ['running', 'seed']
    squanchy        smith           age:21     pet:Rat             hobbies: ['squanching', 'running']
    Eeyore          Milne           age:98     pet:(none)          hobbies: ['Being gloomy, eating thistles and sugar cubes, playing Poohsticks, going to birthday parties, flowers, stars, sad stories and poems, looking over his hill']
    Bat             Man             age:58     pet:(none)          hobbies: ['Justice', 'Seed']
    Muttley         Dog             age:71     pet:Dick Dastardly  hobbies: Laughing
    Scoobert        Doo             age:7      pet:(none)          hobbies: ['Eating Scooby Snacks']
    Sterling        Archer          age:36     pet:Babou           hobbies: ['Drinking', 'Being an ass', 'Super Spy']
    Eric            Cartman         age:10     pet:Mr. Kitty       hobbies: []
    Philip          Fry             age:2040   pet:Nibblonian      hobbies: []

--- people.py
+++ people.py
@@ -107,7 +107,10 @@ def _add_fry(people):
 def main():
     """ main function run as script """
     people = setup_people()
-    print(people)
+    for person in people.values():
+        pet = person.pet or '(none)'
+        print(f'{person.first_name:<15} {person.surname:<15} age:{person.age:<5}'
+              f'  pet:{pet:<15} hobbies: {person.hobbies}')
 
 
 if __name__ == '__main__':
